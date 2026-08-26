56a9a1560a2fe865b0b4f32ce3f8577ce5bd3a02
TayyabIrfan533 <tayyabirfan533@gmail.com>
2026-08-26 18:19:59 +0500

    Add custom login response with user data, verify register/login endpoints

--- backend/apps/users/api/serializers.py
+++ backend/apps/users/api/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 User = get_user_model()
 
 
@@ -21,3 +21,14 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data["user"] = {
+            "id": str(self.user.id),
+            "email": self.user.email,
+            "first_name": self.user.first_name,
+            "last_name": self.user.last_name,
+            "role": self.user.role,
+        }
+        return data
--- backend/apps/users/api/urls.py
+++ backend/apps/users/api/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenBlacklistView,
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
-from .views import MeView, RegisterView
+from .views import MeView, RegisterView, CustomLoginView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", CustomLoginView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("logout/", TokenBlacklistView.as_view(), name="logout"),
     path("me/", MeView.as_view(), name="me"),
